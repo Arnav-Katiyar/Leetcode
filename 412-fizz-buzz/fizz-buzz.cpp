9538476134ec265b8c246befbf1181755efaf70a
Arnav <arnav57742@gmail.com>
2026-07-29 02:16:48 +0530

    Time: 1 ms (54.64%) | Memory: 11.7 MB (48.30%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+
+        vector<string> Output;
+        for(int i=1; i<=n; i++){
+            if(i%15==0){
+                Output.push_back("FizzBuzz");
+                continue;
+            }
+            if(i%3==0){
+                Output.push_back("Fizz");
+                continue;
+            }
+            if(i%5==0){
+                Output.push_back("Buzz");
+                continue;
+            }
+            else {
+                Output.push_back(to_string(i));
+            }
+        }
+        return Output;
+    }
+};
